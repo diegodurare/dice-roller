5162d72547f45acd0ae6bce53a7065a9c2f845e6
diegodurare <diegoduranarellano@gmail.com>
2025-06-02 12:42:46 +0200

    Segundo commit: añadir código base con función de lanzamiento

---
+++ app.py
@@ -0,0 +1,13 @@
+from dice import roll
+import time
+
+def lanzar_dados(amount, sides):
+    resultados = []
+    for i in range(amount):
+        resultado = roll(f'd1{str(sides)}')
+        resultados.append(resultado)
+        print(f"Lanzamiento {i+1} número obtenido {resultado}")
+        time.sleep(5)
+    return resultados
+
+lanzar_dados(amount=5, sides=6)
